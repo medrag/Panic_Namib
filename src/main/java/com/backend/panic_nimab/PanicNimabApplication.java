a0d5601ac1446e657b17ff26d43159a806ab1fde
medrag <raguigmed@gmail.com>
2018-12-06 21:28:41 +0100

    Initial commit

---
+++ src/main/java/com/backend/panic_nimab/PanicNimabApplication.java
@@ -0,0 +1,12 @@
+package com.backend.panic_nimab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanicNimabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PanicNimabApplication.class, args);
+    }
+}
